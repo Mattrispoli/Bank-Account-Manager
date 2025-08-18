3a690347b84b588a992110a4e886e6c72a0e51cc
Matt Rispoli <matthew.rispoli@outlook.com>
2025-08-17 22:49:06 -0400

    Function declarations in header.hpp

--- header.hpp
+++ header.hpp
@@ -12,4 +12,9 @@
 class BankAccount;
 
 int randNum();
+int openingAcc(std::unordered_map<int,BankAccount>& accounts, double openingAmt);
+void bankOps(std::unordered_map<int,BankAccount>& accounts, int accountNumber);
+void startup(std::unordered_map<int, BankAccount> &accounts, const std::string& filename);
+void saveMap(std::unordered_map<int,BankAccount> &accounts, const std::string& filename);
+
 int main();
